96d0ca22fdb5df5189aea923652a61f35bbf5d39
unknown <User@.(none)>
2010-05-03 15:36:02 +0530

    relations

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,2 +1,8 @@
 class Facility < ActiveRecord::Base
+  has_many :facility_tournament_division
+  has_many :account_playing_details
+  has_many :facility_tournament_divisions
+  has_many :divisions, :through => :facility_tournament_divisions
+  
+  
 end

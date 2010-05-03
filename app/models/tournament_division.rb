8106ffa8974e3f27df494a581ded8cf2642b070f
unknown <User@.(none)>
2010-05-03 15:36:50 +0530

    relations

--- app/models/tournament_division.rb
+++ app/models/tournament_division.rb
@@ -1,2 +1,7 @@
 class TournamentDivision < ActiveRecord::Base
+ belongs_to :tournament
+ has_many :tournament_player
+ has_many :facilties_tournament_division
+ has_many :facilties, :through => :faciltity_tournament_division
+ has_many :divisional_games
 end

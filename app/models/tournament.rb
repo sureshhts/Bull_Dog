75176be479be7f4461ae0673c6571c14f48ee509
Relton <relton@guaranteach.com>
2010-05-03 17:14:34 +0530

    category mappings - relton

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,4 +1,6 @@
 class Tournament < ActiveRecord::Base
  has_many :tournament_players
- has_many :tournament_divisions 
+ has_many :tournament_divisions
+ has_many :tournament_category_mappings
+ has_many :categories, :through => :tournament_category_mappings
 end

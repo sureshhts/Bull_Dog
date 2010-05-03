c5f8eaed9390bed3ff94f11704e0ac6c92cf3da4
unknown <User@.(none)>
2010-05-03 11:24:37 +0530

    updates

--- app/controllers/account_profile_controller.rb
+++ app/controllers/account_profile_controller.rb
@@ -5,4 +5,17 @@ layout "player"
   def myprofile
     @profile = AccountProfile.find(params[:id])
   end
+  
+  def update_profile
+  
+  	@profile = AccountProfile.find(params[:id])
+  	
+  	if @profile.update_attributes(params[:profile])
+     
+      flash[:notice] = 'Your Profile was successfully updated.'
+      redirect_to :controller => 'account_profile', :action => 'myprofile', :id => params[:id]
+    else
+      render :action => "myprofile"
+    end
+  end
 end

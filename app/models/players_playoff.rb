4fc41bc1cb07c1580fc44cc46579b8edc22570e5
unknown <User@.(none)>
2010-05-03 15:36:33 +0530

    relations

--- app/models/players_playoff.rb
+++ app/models/players_playoff.rb
@@ -1,2 +1,9 @@
 class PlayersPlayoff < ActiveRecord::Base
+ belongs_to :divisional_game
+ 
+ belongs_to :tournament_player,
+   :foreign_key => 'player',  :class_name => 'TournamentPlayer'
+ belongs_to :tournament_player,
+   :foreign_key => 'opponent',  :class_name => 'TournamentPlayer'
+   
 end

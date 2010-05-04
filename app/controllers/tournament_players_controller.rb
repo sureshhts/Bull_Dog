6cf28bfd2a8eb18d3163b6780b781465c3b8601f
unknown <User@.(none)>
2010-05-04 10:51:55 +0530

    updates

--- app/controllers/tournament_players_controller.rb
+++ app/controllers/tournament_players_controller.rb
@@ -1,2 +1,38 @@
 class TournamentPlayersController < ApplicationController
+
+layout 'player'
+
+ def tour_player_registration
+  @player_levels = PlayerLevel.find(:all)
+  @category = TournamentCategory.find(:all)
+  @tournament = Tournament.find(:all)
+  puts "******user id*******"
+  puts @id = session[:user_id]
+ end
+ 
+ def create
+   if params['commit']  == 'Cancel'
+		   redirect_to :action => ''
+		   return
+	    else
+    	end
+    	
+     tour_id = params[:tournament][:tournament_id]
+     category_id = params[:tournament][:category]
+     level_id = params[:tournament][:player_level]
+     
+     @tournament = Tournament.create(:user_id=> params[:tournament][:user_id], :tournament_id => tour_id, :level_id => level_id, :category_id => category_id)
+    
+    	@reg= TournamentPlayer.create(:user_id=> params[:tournament][:user_id], :tournament_id => tour_id, :level_id => level_id, :category_id => category_id)
+ 	
+  		if @reg.save
+		   flash[:notice] =  'New Player Level Created Successfully'
+		   redirect_to :action => 'tour_player_registration', :controller => 'tournament_player'
+		else
+	  	   render :action => ''
+	    end
+ 
+ end
+ 
+ 
 end

0dd13e32369dfd241197422377e600caa1ba013c
unknown <User@.(none)>
2010-05-04 10:52:18 +0530

    updates

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -24,10 +24,15 @@ layout 'default'
     reg_ends = Time.parse(params[:tournament][:registration_ends])
     tourn_starts = Time.parse(params[:tournament][:tournament_starts])
        
- 	@tournament = Tournament.create(:name => params[:tournament][:name], :city => params[:tournament][:city], :tournament_type => 	params[:tournament][:tournament_type], :kind_of_tournament => params[:tournament][:kind_of_tournament], :registration_starts => reg_starts,
- 	:registration_ends => reg_ends, :tournament_starts => tourn_starts, :category_id => params[:tournament][:category_id])
+ 	@tournament = Tournament.create(:name => params[:tournament][:name], :city => params[:tournament][:city], :tournament_type => 	params[:tournament][:tournament_type], :kind_of_tournament => params[:tournament][:kind_of_tournament], :registration_starts => 	reg_starts,	:registration_ends => reg_ends, :tournament_starts => tourn_starts)
  	
   	if @tournament.save
+  	
+  	#puts params[:tournament][:category_id].inspect
+  	# puts   cats = params[:tournament][:category_id]
+  	   for cat in params[:tournament][:category_id]
+  	    tc_map = TournamentCategoryMapping.create(:tournament_id => @tournament.id, :tournament_category_id => cat.to_i)
+  	   end
 	   flash[:notice] =  'Tournament Created Successfully'
 	   redirect_to :action => 'index', :controller => 'tournaments'
 	else
@@ -39,7 +44,7 @@ layout 'default'
   def edit_tournament
    
   	@tournament = Tournament.find(params[:id])
-    
+    @cat = TournamentCategory.find(:all)
     @reg = Time.at(@tournament.registration_starts).strftime("%B %d, %Y")
     @reg_ends = Time.at(@tournament.registration_ends).strftime("%B %d, %Y")
     @tour_date = Time.at(@tournament.tournament_starts).strftime("%B %d, %Y")
@@ -48,20 +53,27 @@ layout 'default'
   
   def update
   	
-
+   
    @tournament = Tournament.find(params[:id])
    
    reg_starts =  Time.parse(params[:tournament][:registration_starts])
     reg_ends = Time.parse(params[:tournament][:registration_ends])
     tourn_starts = Time.parse(params[:tournament][:tournament_starts])
    
-    puts "****tour*******"
-     puts @tournament.name
-    if @tournament.update_attributes(:name => params[:tournament][:name], :city => params[:tournament][:city], :tournament_type => 	params[:tournament][:tournament_type], :kind_of_tournament => params[:tournament][:kind_of_tournament], :registration_starts => reg_starts,
- 	:registration_ends => reg_ends, :tournament_starts => tourn_starts, :category_id => params[:tournament][:category_id])
+
+    if @tournament.update_attributes(:name => params[:tournament][:name], :city => params[:tournament][:city], :tournament_type => 	params[:tournament][:tournament_type], :kind_of_tournament => params[:tournament][:kind_of_tournament], :registration_starts =>   		reg_starts,	:registration_ends => reg_ends, :tournament_starts => tourn_starts)
+ 	
+     tc_mappings = TournamentCategoryMapping.find(:all, :conditions => ["tournament_id=?", @tournament.id])
+     for tcm in tc_mappings
+      tcm.destroy
+     end
+     
+     for tcm in params[:tournament][:category_id]
+      tc_map = TournamentCategoryMapping.create(:tournament_id => @tournament.id, :tournament_category_id => tcm.to_i)
+     end
      
       flash[:notice] = 'Tournament was successfully updated.'
-      puts "***completed****"
+     
       redirect_to :controller => 'tournaments', :action => 'index'
     else
       render :action => "edit_tournament"

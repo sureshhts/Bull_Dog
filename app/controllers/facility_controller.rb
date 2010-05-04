f42f0244124ab48330438b5c831540d0984e53c7
unknown <User@.(none)>
2010-05-04 14:27:13 +0530

    updates

--- app/controllers/facility_controller.rb
+++ app/controllers/facility_controller.rb
@@ -1,5 +1,11 @@
 class FacilityController < ApplicationController
 
+layout 'default'
+
+  def index
+   @facility = Facility.find(:all)
+  end
+
   def add_facilities
   
   end
@@ -22,5 +28,28 @@ class FacilityController < ApplicationController
 	   render :action => 'new'
 	end
   end
+  
+  def delete
+    Facility.delete(params[:id])
+  	redirect_to :controller => 'facility', :action => 'index'
+  end
+  
+  def edit_facility
+   	@facility = Facility.find(params[:id])
+  end
+  
+  def update
+  
+  	@facility = Facility.find(params[:id])
+  	
+  	if @facility.update_attributes(params[:facility])
+      flash[:notice] = "Facility updated successfully"
+      redirect_to :action => 'index', :controller => 'facility'
+    else
+      flash[:notice] = "Try again"
+      redirect_to :action => 'edit_facility', :controller => 'facility', :id => params[:id]
+    end  
+      
+  end 
 
 end

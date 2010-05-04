6cece06de147db9948f6790dea4585ab11f5c4c4
unknown <User@.(none)>
2010-05-04 14:27:33 +0530

    updates

--- app/controllers/player_level_controller.rb
+++ app/controllers/player_level_controller.rb
@@ -1,25 +1,36 @@
 class PlayerLevelController < ApplicationController
 
-	def add_level
+layout 'default'
+
+  def index
+    @level = PlayerLevel.find(:all)
+  end
+
+  def add_level
 	
+  end
+	
+  def create
+	if params['commit']  == 'Cancel'
+	   redirect_to :action => ''
+	   return
+    else
 	end
-	
-	def create
-		if params['commit']  == 'Cancel'
-		   redirect_to :action => ''
-		   return
-	    else
-    	end
     
     
-    	@level= PlayerLevel.create(params[:level])
- 	
-  		if @level.save
-		   flash[:notice] =  'New Player Level Created Successfully'
-		   redirect_to :action => 'index', :controller => 'player_level'
-		else
-	  	   render :action => 'new'
-	    end
+	@level= PlayerLevel.create(params[:level])
+
+	if @level.save
+	   flash[:notice] =  'New Player Level Created Successfully'
+	   redirect_to :action => 'index', :controller => 'player_level'
+	else
+  	   render :action => 'new'
+    end
+  end
+  
+  def delete
+  	 PlayerLevel.delete(params[:id])
+  	 redirect_to :controller => 'player_level', :action => 'index'
   end
 
 end

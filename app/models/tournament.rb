646edb908739f5ac3525a5cc3de44eb0237c8065
Relton <relton@guaranteach.com>
2010-05-04 12:41:39 +0530

    new changes - relton

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -3,4 +3,8 @@ class Tournament < ActiveRecord::Base
  has_many :tournament_divisions
  has_many :tournament_category_mappings
  has_many :tournament_categories, :through => :tournament_category_mappings
+
+ def self.tournaments_for_registration(user)
+
+ end
 end

7439ba00d97a2a7c734c8d6523469cc0eaaa1235
unknown <User@.(none)>
2010-05-04 18:14:19 +0530

    updates

--- app/controllers/tournament_players_controller.rb
+++ app/controllers/tournament_players_controller.rb
@@ -3,34 +3,39 @@ class TournamentPlayersController < ApplicationController
 layout 'player'
 
  def tour_player_registration
-  @player_levels = PlayerLevel.find(:all)
-  @category = TournamentCategory.find(:all)
-  @tournament = Tournament.find(:all)
-  puts "******user id*******"
-  puts @id = session[:user_id]
- end
+    @id = session[:user_id]
+    id = params[:id]
+    @tournament = Tournament.find(id)
+    @level_id = User.find(session[:user_id]).account_playing_detail.player_level_id
+    @player_levels = PlayerLevel.find(:all)
+    @category = @tournament.tournament_categories
+  end
  
  def create
+ 
    if params['commit']  == 'Cancel'
-		   redirect_to :action => ''
-		   return
-	    else
-    	end
+	  redirect_to :action => ''
+	  return
+   else
+   end
     	
      tour_id = params[:tournament][:tournament_id]
-     category_id = params[:tournament][:category]
+     puts  category_id = params[:tournament][:category]
+    puts category_id.length
      level_id = params[:tournament][:player_level]
      
-     @tournament = Tournament.create(:user_id=> params[:tournament][:user_id], :tournament_id => tour_id, :level_id => level_id, :category_id => category_id)
     
-    	@reg= TournamentPlayer.create(:user_id=> params[:tournament][:user_id], :tournament_id => tour_id, :level_id => level_id, :category_id => category_id)
+     for cat in params[:tournament][:category]
+  	    @reg= TournamentPlayer.create(:user_id=> params[:tournament][:user_id], :tournament_id => tour_id, :player_level_id => level_id,  		:category_id => cat)	
+  	 end
+    
  	
-  		if @reg.save
-		   flash[:notice] =  'New Player Level Created Successfully'
-		   redirect_to :action => 'tour_player_registration', :controller => 'tournament_player'
-		else
-	  	   render :action => ''
-	    end
+  	if @reg
+	   flash[:notice] =  'New Player Level Created Successfully'
+	   redirect_to :action => 'index', :controller => 'registration'
+	else
+	   render :action => 'tour_player_registration', :controller => 'tournament_players'
+	end
  
  end
  

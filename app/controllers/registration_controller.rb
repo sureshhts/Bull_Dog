339c5fddc7a32593b9a3b969b481d425bfc518ee
Relton <relton@guaranteach.com>
2010-05-04 13:40:19 +0530

    registration - relton

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -2,6 +2,6 @@ class RegistrationController < ApplicationController
   layout "player"
 
   def index
-    
+    @tournaments = Tournament.tournaments_for_registration(session[:user_id])
   end
 end

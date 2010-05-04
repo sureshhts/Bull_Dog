82f407eccc23a2df3dad1ecb3a113492cc2e234e
Relton <relton@guaranteach.com>
2010-05-04 12:43:14 +0530

    new chnages - relton

--- db/migrate/20100422082145_create_divisional_games.rb
+++ db/migrate/20100422082145_create_divisional_games.rb
@@ -3,22 +3,18 @@ class CreateDivisionalGames < ActiveRecord::Migration
    execute %Q(
     create table divisional_games(
 	id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-	division_id INT(11) UNSIGNED not NULL,
+	tournament_division_id INT(11) UNSIGNED not NULL,
 	week_number INT(11) UNSIGNED not NULL,
 	starts INT(11) UNSIGNED not null,
 	ends INT(11) UNSIGNED not null,
-	player1 INT(11) UNSIGNED not null,
-	player2 INT(11) UNSIGNED not null,
 	player1_set1 INT(11) UNSIGNED default NULL,
 	player1_set2 INT(11) UNSIGNED default NULL,
 	player1_set3 INT(11) UNSIGNED default NULL,
 	player2_set1 INT(11) UNSIGNED default NULL,
 	player2_set2 INT(11) UNSIGNED default NULL,
 	player2_set3 INT(11) UNSIGNED default NULL,
-	Primary key(id),
-	Foreign key(player1) references tournament_players(id) on delete cascade on update cascade,
-	Foreign key(player2) references tournament_players(id) on delete cascade on update cascade,
-	Foreign key(division_id) references tournament_divisions(id) on delete cascade on update cascade
+	Primary key(id),	
+	Foreign key(tournament_division_id) references tournament_divisions(id) on delete cascade on update cascade
 	)Engine = InnoDB;
   )
   end

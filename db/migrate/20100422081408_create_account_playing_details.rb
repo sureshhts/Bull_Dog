7a175f07b050edc1849ac08e80b707dbe0af411a
Relton <relton@guaranteach.com>
2010-05-04 12:42:30 +0530

    new changes - relton

--- db/migrate/20100422081408_create_account_playing_details.rb
+++ db/migrate/20100422081408_create_account_playing_details.rb
@@ -5,12 +5,12 @@ class CreateAccountPlayingDetails < ActiveRecord::Migration
 	id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
 	user_id INT(11) UNSIGNED NOT NULL,
 	facility_id INT(11) UNSIGNED NOT NULL,
-	level_id INT(11) UNSIGNED NOT NULL,
+	player_level_id INT(11) UNSIGNED NOT NULL,
 	verified enum('Yes','No') not null default 'No',
 	Primary key(id),
 	Foreign key(user_id) references users(id) on delete cascade on update cascade,
 	Foreign key(facility_id) references facilities(id) on delete cascade on update cascade,
-	Foreign key(level_id) references player_levels(id) on delete cascade on update cascade
+	Foreign key(player_level_id) references player_levels(id) on delete cascade on update cascade
 	)Engine = InnoDB;
   )
   end

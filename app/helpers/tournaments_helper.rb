41ef341667ad684bd3c0961e9ef560193529e45e
Relton <relton@guaranteach.com>
2010-05-10 18:29:33 +0530

    tp players - relton

--- app/helpers/tournaments_helper.rb
+++ app/helpers/tournaments_helper.rb
@@ -1,2 +1,14 @@
 module TournamentsHelper
+  def sort_table_header(text, param, action)
+    key = param
+    key += "_reverse" if params[:sort] == param
+    options = {
+        :url => {:action => action, :params => params.merge({:sort => key, :page => nil})}
+    }
+    html_options = {
+      :title => "Sort by this field",
+      :href => url_for(:action => action, :params => params.merge({:sort => key, :page => nil}))
+    }
+    link_to_remote(text, options, html_options)
+  end
 end

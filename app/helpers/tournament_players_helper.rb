12b3d20772ed304a21e5d82e92aaf1bc585e22eb
Relton <relton@guaranteach.com>
2010-05-10 11:43:48 +0530

    tournament players page - relton

--- app/helpers/tournament_players_helper.rb
+++ app/helpers/tournament_players_helper.rb
@@ -1,2 +1,10 @@
 module TournamentPlayersHelper
+  def tournament_next_action(t_id)
+    tournament = Tournament.find(t_id)
+    if tournament.tournament_type == "L"
+      return "league_division"
+    elsif tournament.tournament_type == "K"
+      return "knockout_points"
+    end
+  end
 end

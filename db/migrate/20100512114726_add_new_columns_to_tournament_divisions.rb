41ec95dda6fcc60548585d6d4c54690920231f7f
Relton <relton@guaranteach.com>
2010-05-12 17:27:28 +0530

    new columns to tournament divisions

---
+++ db/migrate/20100512114726_add_new_columns_to_tournament_divisions.rb
@@ -0,0 +1,20 @@
+class AddNewColumnsToTournamentDivisions < ActiveRecord::Migration
+  def self.up
+    execute %Q{  ALTER TABLE `tournament_divisions` ADD COLUMN `tournament_category_id` INT(11) UNSIGNED AFTER `no_of_players`,
+                 ADD INDEX `Index_tournament_category_id` USING BTREE(`tournament_category_id`),
+                 ADD CONSTRAINT `FK_tournament_divisions_2` FOREIGN KEY `FK_tournament_divisions_2` (`tournament_category_id`)
+                    REFERENCES `tournament_categories` (`id`)
+                    ON DELETE CASCADE
+                    ON UPDATE CASCADE }
+
+    execute %Q{  ALTER TABLE `tournament_divisions` ADD COLUMN `player_level_id` INT(11) UNSIGNED AFTER `tournament_category_id`,
+                 ADD INDEX `Index_player_level_id` USING BTREE(`player_level_id`),
+                 ADD CONSTRAINT `FK_tournament_divisions_3` FOREIGN KEY `FK_tournament_divisions_3` (`player_level_id`)
+                    REFERENCES `player_levels` (`id`)
+                    ON DELETE CASCADE
+                    ON UPDATE CASCADE}
+  end
+
+  def self.down
+  end
+end

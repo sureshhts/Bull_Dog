a0f5da79ee9f959da62b28a7e4b80194b380169f
Relton <relton@guaranteach.com>
2010-05-17 11:34:37 +0530

    new changes - relton

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -41,8 +41,12 @@ class Tournament < ActiveRecord::Base
     find_by_sql(query)
  end
 
- def self.tournament_players_summary(tournament_id, sort)
-   query = %Q{  SELECT tp.id as player_id, acp.user_id as user_id, concat(acp.first_name,' ',acp.last_name) as name, tc.name as category_name, pl.name as player_level, f.name as facility_name
+ def self.tournament_players_summary(tournament_id, sort, *conditions)
+   condition = ""
+   if !conditions[0].blank?
+     condition = " and tc.id = #{conditions[0][0]} and pl.id = #{conditions[0][1]}"
+   end
+   query = %Q{  SELECT tp.id as player_id, acp.user_id as user_id, concat(acp.first_name,' ',acp.last_name) as name, tc.name as category_name, pl.name as player_level, f.name as facility_name, td.name as division_name
                 from tournaments t
                 join tournament_players tp on tp.tournament_id = t.id
                 join account_profiles acp on acp.user_id = tp.user_id
@@ -50,19 +54,50 @@ class Tournament < ActiveRecord::Base
                 join player_levels pl on pl.id = tp.player_level_id
                 join account_playing_details apd on apd.user_id = tp.user_id
                 join facilities f on f.id = apd.facility_id
-                where t.id = #{tournament_id}
+                left outer join tournament_divisions td on td.id = tp.tournament_division_id
+                where t.id = #{tournament_id} #{condition}
                 order by #{sort}}
     find_by_sql(query)
  end
 
- def tournament_divisions
-   query = %Q{  SELECT d.id, d.name, d.no_of_players, f.name as facility
+ def self.tournament_divisions(tournament_id, *conditions)
+   condition = ""
+   if !conditions[0].blank?
+     condition = " and tc.id = #{conditions[0][0]} and pl.id = #{conditions[0][1]}"
+   end
+   query = %Q{  SELECT d.id, d.name, d.no_of_players, f.name as facility, d.area_name, count(tp.id) as players
                 from tournaments t
                 join tournament_divisions d on d.tournament_id = t.id
                 join facilities_tournament_divisions ftd on ftd.tournament_division_id = d.id
                 join facilities f on f.id = ftd.facility_id
-                where t.id = #{@id} }
+                join tournament_categories tc on tc.id = d.tournament_category_id
+                join player_levels pl on pl.id = d.player_level_id
+                left outer join tournament_players tp on tp.tournament_division_id = d.id
+                where t.id = #{tournament_id} #{condition}
+                group by d.id }
 
+   find_by_sql(query)
+ end
+
+ def self.tournament_division_players(division)
+   query = %Q{  SELECT tp.id as player_id, concat(acp.first_name,' ',acp.last_name) as name
+                from tournament_players tp
+                join tournament_divisions td on td.id = tp.tournament_division_id
+                join account_profiles acp on acp.user_id = tp.user_id
+                join account_playing_details apd on apd.user_id = tp.user_id
+                where td.id = #{division} }
+   find_by_sql(query)
+ end
+
+ def self.tournament_categories_levels(tournament_id, sort)
+   query = %Q{  select tc.name as category, tc.id as category_id, pl.name as player_level, pl.id as level_id, count(tp.id) as total_players, count(case when tp.tournament_division_id > 0 then 1 end ) as allotted_players
+                from tournaments t
+                join tournament_players tp on tp.tournament_id = t.id
+                left outer join tournament_categories tc on tc.id = tp.tournament_category_id
+                left outer join player_levels pl on pl.id = tp.player_level_id
+                where t.id = #{tournament_id}
+                group by tc.id, pl.id
+                order by #{sort} }
    find_by_sql(query)
  end
 end

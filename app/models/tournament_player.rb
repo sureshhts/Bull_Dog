7c96291a125f541b310701cb56ab493e4fb5ac04
Relton <relton@guaranteach.com>
2010-05-19 18:21:18 +0530

    schedules for divisions - relton

--- app/models/tournament_player.rb
+++ app/models/tournament_player.rb
@@ -4,10 +4,8 @@ class TournamentPlayer < ActiveRecord::Base
  belongs_to :tournament_category
  belongs_to :player_level
  belongs_to :tournament_division
-  
- has_one :player,
-   :foreign_key => 'player',  :class_name => 'player_playoff'
- has_one :player,
-   :foreign_key => 'opponent',  :class_name => 'player_playoff'
-   
+ has_many :winners, :class_name => 'LeagueScheduleGame', :foreign_key => 'winner'
+ has_many :losers, :class_name => 'LeagueScheduleGame', :foreign_key => 'loser'
+ has_many :league_game_players
+ has_many :league_schedule_games, :through => :league_game_players
 end

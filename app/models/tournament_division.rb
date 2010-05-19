6a0d22b114491656dcf3d9700b02b6a150d8dcac
Relton <relton@guaranteach.com>
2010-05-19 18:21:07 +0530

    schedules for divisions - relton

--- app/models/tournament_division.rb
+++ app/models/tournament_division.rb
@@ -1,7 +1,7 @@
 class TournamentDivision < ActiveRecord::Base
  belongs_to :tournament
  has_many :tournament_player
+ has_many :tournament_division_league_schedules
  has_many :facilties_tournament_division
  has_many :facilties, :through => :faciltity_tournament_division
- has_many :divisional_games
 end

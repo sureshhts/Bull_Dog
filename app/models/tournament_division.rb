c98dc95bc666b867cd661b417f703973274cf1c3
Relton <relton@guaranteach.com>
2010-05-20 17:47:59 +0530

    games - relton

--- app/models/tournament_division.rb
+++ app/models/tournament_division.rb
@@ -4,4 +4,16 @@ class TournamentDivision < ActiveRecord::Base
  has_many :tournament_division_league_schedules
  has_many :facilties_tournament_division
  has_many :facilties, :through => :faciltity_tournament_division
+
+  def self.division_with_max_players(tournament_id, player_id, category_id)
+    query = %Q{ select td.id, count(tp.id) as players
+                from tournament_divisions td
+                join tournament_players tp on tp.tournament_division_id = td.id
+                where tp.tournament_id = #{tournament_id} and tp.player_level_id = #{player_id} and tp.tournament_category_id = #{category_id}
+                group by td.id
+                order by players desc
+                limit 1}
+    result = find_by_sql(query)[0]
+    return result.players
+  end
 end

ecca51ae63b994bc3a8dffe6e753c85f1d15d6ae
Relton <relton@guaranteach.com>
2010-05-26 12:33:23 +0530

    score page - relton

--- app/controllers/players_playoffs_controller.rb
+++ app/controllers/players_playoffs_controller.rb
@@ -7,8 +7,54 @@ layout 'player'
   end
   
   def score
-   @categories = TournamentCategory.find(:all)
- 
+   @tournaments = Tournament.find(:all) 
+  end
+
+  def playoff_tournament
+    @tp = TournamentPlayer.find_by_sql("select * from tournament_players where tournament_id='#{params[:tournament]}' and user_id='#{session[:user_id]}'")[0]
+    @tp_division = @tp.tournament_division
+    @tp_division_schedules = @tp_division.tournament_division_league_schedules
+    if request.xml_http_request?
+      respond_to do |format|
+        format.html
+        format.js {
+          render :update do |page|
+            page.replace_html 'div1',:partial => "player_score_div1"
+          end
+        }
+      end
+    end
+  end
+
+  def playoff_tournament_schedule
+    tp_id, td_league_schedule_id = params[:schedule].split(":")
+    @tp = TournamentPlayer.find(tp_id)
+    league_schedule_games = @tp.league_schedule_games
+    @selected_lsg = nil
+    for lsg in league_schedule_games
+      if lsg.tournament_division_league_schedule_id.to_s == td_league_schedule_id.to_s
+        @selected_lsg = lsg
+        break
+      end
+    end
+    stps = @selected_lsg.tournament_players
+    @opponent = nil
+    for stp in stps
+      if stp.id != @tp.id
+        @opponent = stp
+      end
+    end
+    @opponent_profile = @opponent.user.account_profile
+    if request.xml_http_request?
+      respond_to do |format|
+        format.html
+        format.js {
+          render :update do |page|
+            page.replace_html 'div2',:partial => "player_score_div2"
+          end
+        }
+      end
+    end
   end
   
   def playoff_scores

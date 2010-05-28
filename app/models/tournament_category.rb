ed440f23416827936072f654a476fde6abbb8ea6
Relton <relton@guaranteach.com>
2010-05-28 12:10:32 +0530

    score page - relton

--- app/models/tournament_category.rb
+++ app/models/tournament_category.rb
@@ -1,5 +1,6 @@
 class TournamentCategory < ActiveRecord::Base
   has_many :tournament_players
   has_many :tournament_category_mappings
+  has_many :tournament_divisions
   has_many :tournaments, :through => :tournament_category_mappings
 end

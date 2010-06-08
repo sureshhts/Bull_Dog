0e86958c000d5da93540d160bccd7697dc782d6f
Relton <relton@guaranteach.com>
2010-06-08 12:58:21 +0530

    knockout - relton

--- app/models/tournament_player.rb
+++ app/models/tournament_player.rb
@@ -6,8 +6,12 @@ class TournamentPlayer < ActiveRecord::Base
  belongs_to :tournament_division
  has_many :winners, :class_name => 'LeagueScheduleGame', :foreign_key => 'winner'
  has_many :losers, :class_name => 'LeagueScheduleGame', :foreign_key => 'loser'
+ has_many :knockout_winners, :class_name => 'KnockoutGame', :foreign_key => 'winner'
+ has_many :knockout_losers, :class_name => 'KnockoutGame', :foreign_key => 'loser'
  has_many :league_game_players
  has_many :league_schedule_games, :through => :league_game_players
+ has_many :knockout_game_players
+ has_many :knockout_games, :through => :knockout_game_players
 
   def self.tournament_players_league_standings(tournament)
     query = %Q{ select tp.id, concat(ap.first_name,' ',ap.last_name) as name, ap.contact_number, ap.email_address, tp.points, tp.knockout, tp.user_id

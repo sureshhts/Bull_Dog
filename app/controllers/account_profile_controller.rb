3e9c89a1082d455058d0e2ae0cf60e77353b6b79
Relton <relton@guaranteach.com>
2010-06-16 17:28:12 +0530

    my division standings - relton

--- app/controllers/account_profile_controller.rb
+++ app/controllers/account_profile_controller.rb
@@ -27,6 +27,23 @@ layout "player"
   def my_schedule
     @month, @year, @schedules = all_tournaments_for_this_month
   end
+
+  def my_division_standings
+    @tournaments = Tournament.find(:all)
+    @levels = PlayerLevel.find(:all)
+    @categories = TournamentCategory.find(:all)
+    if request.xml_http_request?
+      @tournament_divisions = TournamentDivision.find_by_sql("select * from tournament_divisions where tournament_id = #{params[:tournament]} and tournament_category_id = #{params[:category]} and player_level_id = #{params[:level]}")
+      respond_to do |format|
+        format.html
+        format.js {
+          render :update do |page|
+            page.replace_html 'my_div_stand',:partial => "my_division_standings"
+          end
+        }
+      end
+    end
+  end
   
   def list
    

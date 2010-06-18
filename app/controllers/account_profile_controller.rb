5754a7869ff9147cb669e03f809c6960a5db72a1
Relton <relton@guaranteach.com>
2010-06-18 12:37:27 +0530

    my division standings - relton

--- app/controllers/account_profile_controller.rb
+++ app/controllers/account_profile_controller.rb
@@ -29,7 +29,34 @@ layout "player"
   end
 
   def my_division_standings
-    @tournaments = Tournament.find(:all)
+    @tournaments = Tournament.find_by_sql("select * from tournaments where tournament_type = 'L'")
+    @levels = PlayerLevel.find(:all)
+    @categories = TournamentCategory.find(:all)
+    if request.xml_http_request?
+      @tournament_divisions = Array.new
+      tds = TournamentDivision.find_by_sql("select * from tournament_divisions where tournament_id = #{params[:tournament]} and tournament_category_id = #{params[:category]} and player_level_id = #{params[:level]}")
+      tp = TournamentPlayer.find_by_sql("select * from tournament_players where user_id = #{session[:user_id]} and tournament_id = #{params[:tournament]}")[0]
+      for td in tds
+        td_players = Tournament.tournament_division_players(td.id)
+        td_pids = td_players.collect{|rec| rec.player_id.to_s}
+        if td_pids.include?(tp.id.to_s)
+          @tournament_divisions.push(td)
+        end
+      end
+
+      respond_to do |format|
+        format.html
+        format.js {
+          render :update do |page|
+            page.replace_html 'my_div_stand',:partial => "my_division_standings"
+          end
+        }
+      end
+    end
+  end
+
+  def division_standings
+    @tournaments = Tournament.find_by_sql("select * from tournaments where tournament_type = 'L'")
     @levels = PlayerLevel.find(:all)
     @categories = TournamentCategory.find(:all)
     if request.xml_http_request?
@@ -38,7 +65,7 @@ layout "player"
         format.html
         format.js {
           render :update do |page|
-            page.replace_html 'my_div_stand',:partial => "my_division_standings"
+            page.replace_html 'div_stand',:partial => "division_standings"
           end
         }
       end
@@ -58,6 +85,22 @@ layout "player"
           end
         }
       end
+    end
+  end
+
+  def change_my_availability
+    tp = TournamentPlayer.find(params[:id])
+    available_tp = TournamentPlayer.available_knockout_non_selected_player(tp.tournament_id, tp.points)
+    tp.update_attributes(:knockout => '0')
+    available_tp.update_attributes(:knockout => '1')
+    @players = TournamentPlayer.tournament_players_league_standings(tp.tournament_id)
+    respond_to do |format|
+      format.html
+      format.js {
+        render :update do |page|
+            page.replace_html 'my_play_off',:partial => "my_playoff_standings"
+        end
+      }
     end
   end
   

46f229ef61595eb3f3f22501ff59be9115513deb
Relton <relton@guaranteach.com>
2010-06-18 18:20:25 +0530

    standings - relton

--- app/models/tournament_player.rb
+++ app/models/tournament_player.rb
@@ -23,6 +23,16 @@ class TournamentPlayer < ActiveRecord::Base
     find_by_sql(query)
   end
 
+  def self.tournament_players_level_standings(tournament, level)
+    query = %Q{ select tp.id, concat(ap.first_name,' ',ap.last_name) as name, ap.contact_number, ap.email_address, tp.points, tp.knockout, tp.user_id
+                from tournament_players tp
+                join users u on u.id = tp.user_id
+                join account_profiles ap on u.id = ap.user_id
+                where tp.tournament_id = #{tournament} and player_level_id = #{level}
+                order by points desc }
+    find_by_sql(query)
+  end
+
   def self.available_knockout_non_selected_player(tournament, points)
     query = %Q{ select tp.id
                 from tournament_players tp
@@ -32,5 +42,15 @@ class TournamentPlayer < ActiveRecord::Base
                 order by points desc
                 limit 1}
     return TournamentPlayer.find(find_by_sql(query)[0].id)
+  end
+
+  def self.player_history(player)
+    query = %Q{ select tp.id, t.name, pl.name as level, tc.name as category, tp.points
+                from tournament_players tp
+                join tournaments t on t.id = tp.tournament_id
+                join player_levels pl on pl.id = tp.player_level_id
+                join tournament_categories tc on tc.id = tp.tournament_category_id
+                where tp.user_id = #{player}}
+    find_by_sql(query)
   end
 end

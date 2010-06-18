81e110bf1e53a80e532d1709e1536d7cbf0f7501
unknown <User@.(none)>
2010-06-18 18:47:46 +0530

    tour_photos

--- app/models/account_profile.rb
+++ app/models/account_profile.rb
@@ -1,3 +1,12 @@
+require 'paperclip'
+
 class AccountProfile < ActiveRecord::Base
   belongs_to :user
+  
+  has_attached_file :photo, 
+     :styles => { :medium => "300x300>",
+     :thumb => "50x50>"},
+     :path => "c:\\ruby\\bulldog_dev\\Bull_Dog\\public\\photos\\drafts\\:id\\:style\\:basename.:extension",
+     :url => "http://localhost:3000/photos/drafts/:id/:style/:basename.:extension"
+
 end

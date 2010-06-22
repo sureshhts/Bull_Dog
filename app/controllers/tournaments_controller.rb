8bfe2ea0ba4c773815eab3f7e419b805beda72e8
unknown <User@.(none)>
2010-06-22 16:31:46 +0530

    updates

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -18,8 +18,9 @@ protect_from_forgery :only => [:destroy]
 
     reg_starts =  Time.parse(params[:tournament][:registration_starts])
     reg_ends = Time.parse(params[:tournament][:registration_ends])
-    tourn_starts = Time.parse(params[:tournament][:tournament_starts])       
- 	  @tournament = Tournament.create(:name => params[:tournament][:name], :city => params[:tournament][:city], :tournament_type => 	params[:tournament][:tournament_type], :kind_of_tournament => params[:tournament][:kind_of_tournament], :registration_starts => 	reg_starts,	:registration_ends => reg_ends, :tournament_starts => tourn_starts)
+    tourn_starts = Time.parse(params[:tournament][:tournament_starts])  
+    tourn_ends =  Time.parse(params[:tournament][:tournament_ends])      
+ 	  @tournament = Tournament.create(:name => params[:tournament][:name], :city => params[:tournament][:city], :tournament_type => 	params[:tournament][:tournament_type], :kind_of_tournament => params[:tournament][:kind_of_tournament], :registration_starts => reg_starts,	:registration_ends => reg_ends, :tournament_starts => tourn_starts, :tournament_starts => tourn_ends)
 
     if @tournament.save
       for cat in params[:tournament][:category_id]

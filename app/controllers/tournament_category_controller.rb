abb8658baf98be16a4e807e188f3afae45456fb2
unknown <User@.(none)>
2010-06-23 11:33:11 +0530

    updates

--- app/controllers/tournament_category_controller.rb
+++ app/controllers/tournament_category_controller.rb
@@ -29,6 +29,22 @@ layout 'default'
 	end
  end
  
+ def edit_category
+  @category = TournamentCategory.find(params[:id])
+ 
+ end
+ 
+ def update
+  @category = TournamentCategory.find(params[:id])
+  
+   if @category.update_attributes(params[:level])
+      flash[:notice] = 'Player Level updated successfully.'
+      redirect_to :action => 'index'
+    else
+      redirect_to :action => 'edit_player_lavel', :id => 'params[:id]'
+    end
+ end
+ 
  def delete
   	 TournamentCategory.delete(params[:id])
   	 redirect_to :controller => 'tournament_category', :action => 'index'

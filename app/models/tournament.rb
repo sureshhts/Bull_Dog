f5f7d3ebb011218d1518f411144ed34173381782
Relton <relton@guaranteach.com>
2010-06-24 11:57:27 +0530

    ko changes - relton

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -2,6 +2,7 @@ class Tournament < ActiveRecord::Base
  has_many :tournament_players
  has_many :tournament_divisions
  has_many :knockout_games
+ has_many :knockout_rounds
  has_many :tournament_category_mappings
  has_many :tournament_categories, :through => :tournament_category_mappings
 

1e56b0ac378bdc47073e2c2fee7a61e667cb9764
unknown <User@.(none)>
2010-06-24 18:51:50 +0530

    manju changes - relton

--- app/models/tournament_photo.rb
+++ app/models/tournament_photo.rb
@@ -3,7 +3,7 @@ class TournamentPhoto < ActiveRecord::Base
 has_attached_file :photo, 
      :styles => { :medium => "300x300>",
      :thumb => "50x50>"},
-     :path => "c:\\ruby\\bulldog_dev\\Bull_Dog\\public\\photos\\drafts\\:id\\:style\\:basename.:extension",
-     :url => "http://localhost:3000/photos/drafts/:id/:style/:basename.:extension"
+     :path => "http:\\tennis.hibiscustech.com\\images\\photos\\drafts\\:id\\:style\\:basename.:extension",
+     :url => "http://tennis.hibiscustech.com/images/photos/drafts/:id/:style/:basename.:extension"
 
 end

61568ae86f10da110776f8278d0243b85c93246d
Relton <relton@guaranteach.com>
2010-06-24 11:57:42 +0530

    ko changes - relton

--- app/models/knockout_game.rb
+++ app/models/knockout_game.rb
@@ -4,6 +4,7 @@ class KnockoutGame < ActiveRecord::Base
   belongs_to :loser, :class_name => "TournamentPlayer", :foreign_key => 'loser'
   has_many :knockout_game_players
   has_many :tournament_players, :through => :knockout_game_players
+  belongs_to :knockout_round
   
   #self-join for parent and child relationship
   has_many :knockouts, :class_name => "KnockoutGame", :foreign_key => "parent_game_id"
